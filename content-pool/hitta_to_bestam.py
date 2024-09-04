2459c2d0fb9ed72495c4eab7c67c7e775ef79f8d
Yerin Kwak <kwak@berkeley.edu>
2024-09-04 01:03:49 -0700

    update Hitta to Bestam

---
+++ content-pool/hitta_to_bestam.py
@@ -0,0 +1,37 @@
+import os
+import json
+import re
+
+def replace_text_in_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    
+    json_str = json.dumps(data, ensure_ascii=False)
+
+    json_str = re.sub(r'(^|\:\s*")Hitta\b', r'\1Bestäm', json_str, flags=re.MULTILINE)
+    json_str = re.sub(r'(?<=\.\s)Hitta\b', 'Bestäm', json_str)
+
+    
+    data = json.loads(json_str)
+    
+    with open(file_path, 'w', encoding='utf-8') as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
+
+    print(f"Processed file: {file_path}")  
+
+def process_folder(folder_path):
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            if file.endswith('.json'):
+                file_path = os.path.join(root, file)
+                replace_text_in_file(file_path)
+
+def main():
+    content_pool_path = '/home/yerin/OATutor-Content-Swe-v2/content-pool'
+
+  
+    process_folder(content_pool_path)
+
+if __name__ == "__main__":
+    main()
+
